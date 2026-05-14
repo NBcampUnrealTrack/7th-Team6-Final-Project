cbd096895ea92a373c01f6fc6ade5d089ccd5524
Yahoo <sjffhdfg@gmail.com>
2026-05-14 17:48:07 +0900

    project init

---
+++ PartTimeBeat/Intermediate/Build/Win64/UnrealEditor/Inc/PartTimeBeat/UHT/PartTimeBeatGameMode.generated.h
@@ -0,0 +1,60 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "PartTimeBeatGameMode.h"
+
+#ifdef PARTTIMEBEAT_PartTimeBeatGameMode_generated_h
+#error "PartTimeBeatGameMode.generated.h already included, missing '#pragma once' in PartTimeBeatGameMode.h"
+#endif
+#define PARTTIMEBEAT_PartTimeBeatGameMode_generated_h
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+// ********** Begin Class APartTimeBeatGameMode ****************************************************
+PARTTIMEBEAT_API UClass* Z_Construct_UClass_APartTimeBeatGameMode_NoRegister();
+
+#define FID_Users_dignw_Documents_GitHub_7th_Team6_Final_Project_PartTimeBeat_Source_PartTimeBeat_PartTimeBeatGameMode_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAPartTimeBeatGameMode(); \
+	friend struct Z_Construct_UClass_APartTimeBeatGameMode_Statics; \
+	static UClass* GetPrivateStaticClass(); \
+	friend PARTTIMEBEAT_API UClass* Z_Construct_UClass_APartTimeBeatGameMode_NoRegister(); \
+public: \
+	DECLARE_CLASS2(APartTimeBeatGameMode, AGameModeBase, COMPILED_IN_FLAGS(CLASS_Abstract | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/PartTimeBeat"), Z_Construct_UClass_APartTimeBeatGameMode_NoRegister) \
+	DECLARE_SERIALIZER(APartTimeBeatGameMode)
+
+
+#define FID_Users_dignw_Documents_GitHub_7th_Team6_Final_Project_PartTimeBeat_Source_PartTimeBeat_PartTimeBeatGameMode_h_15_ENHANCED_CONSTRUCTORS \
+	/** Deleted move- and copy-constructors, should never be used */ \
+	APartTimeBeatGameMode(APartTimeBeatGameMode&&) = delete; \
+	APartTimeBeatGameMode(const APartTimeBeatGameMode&) = delete; \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APartTimeBeatGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APartTimeBeatGameMode); \
+	DEFINE_ABSTRACT_DEFAULT_CONSTRUCTOR_CALL(APartTimeBeatGameMode) \
+	NO_API virtual ~APartTimeBeatGameMode();
+
+
+#define FID_Users_dignw_Documents_GitHub_7th_Team6_Final_Project_PartTimeBeat_Source_PartTimeBeat_PartTimeBeatGameMode_h_12_PROLOG
+#define FID_Users_dignw_Documents_GitHub_7th_Team6_Final_Project_PartTimeBeat_Source_PartTimeBeat_PartTimeBeatGameMode_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_dignw_Documents_GitHub_7th_Team6_Final_Project_PartTimeBeat_Source_PartTimeBeat_PartTimeBeatGameMode_h_15_INCLASS_NO_PURE_DECLS \
+	FID_Users_dignw_Documents_GitHub_7th_Team6_Final_Project_PartTimeBeat_Source_PartTimeBeat_PartTimeBeatGameMode_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+class APartTimeBeatGameMode;
+
+// ********** End Class APartTimeBeatGameMode ******************************************************
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_dignw_Documents_GitHub_7th_Team6_Final_Project_PartTimeBeat_Source_PartTimeBeat_PartTimeBeatGameMode_h
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
