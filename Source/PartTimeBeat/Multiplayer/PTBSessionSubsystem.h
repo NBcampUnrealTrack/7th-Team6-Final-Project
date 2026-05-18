3621f789b32f1061b5aa9182e4933ea3f3e5f496
DJKIM2002 <op302384@gmail.com>
2026-05-18 19:33:44 +0900

    feat: PTBSessionSubsystem 클래스 작성

--- Source/PartTimeBeat/Multiplayer/PTBSessionSubsystem.h
+++ Source/PartTimeBeat/Multiplayer/PTBSessionSubsystem.h
@@ -7,11 +7,30 @@
 #include "PTBSessionSubsystem.generated.h"
 
 /**
- * 
+ * Steam/LAN 세션 생성, 검색, 참가를 담당하는 서브시스템
  */
 UCLASS()
 class PARTTIMEBEAT_API UPTBSessionSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "PTB|Multiplayer")
+	void CreateSession(int32 InMaxPlayers, bool bIsLANMatch);
+
+	UFUNCTION(BlueprintCallable, Category = "PTB|Multiplayer")
+	void FindSessions(int32 MaxSearchResults, bool bIsLANQuery);
+
+	UFUNCTION(BlueprintCallable, Category = "PTB|Multiplayer")
+	void JoinSessionByIndex(int32 SessionIndex);
+
+public:
+	UPROPERTY(BlueprintReadOnly, Category = "PTB|Multiplayer")
+	bool bIsHost = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "PTB|Multiplayer")
+	int32 MaxPlayers = 4;
+
+	UPROPERTY(BlueprintReadOnly, Category = "PTB|Multiplayer")
+	FName CurrentSessionName = NAME_None;
 };
