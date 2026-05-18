697ad37dee99c8a410880cad425df55054e33708
limyt0 <dudx45@gmail.com>
2026-05-18 12:02:19 +0900

    C++ file 추가

---
+++ Source/PartTimeBeat/Core/PTBGameInstance.cpp
@@ -0,0 +1,2 @@
+#include "Core/PTBGameInstance.h"
+
---
+++ Source/PartTimeBeat/Multiplayer/PTBResultVerifier.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "PTBResultVerifier.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARTTIMEBEAT_API UPTBResultVerifier : public UObject
+{
+	GENERATED_BODY()
+	
+};
