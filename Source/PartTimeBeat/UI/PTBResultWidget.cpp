2f70176efcf0c2145ab031c3499dae504497d050
DJKIM2002 <op302384@gmail.com>
2026-05-18 17:31:09 +0900

    feat: PTBResultWidget

--- Source/PartTimeBeat/UI/PTBResultWidget.cpp
+++ Source/PartTimeBeat/UI/PTBResultWidget.cpp
@@ -1,4 +1,13 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PTBResultWidget.h"
+
+void UPTBResultWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	InitializeView();
+}
+
+void UPTBResultWidget::InitializeView()
+{
+}
--- Source/PartTimeBeat/UI/PTBResultWidget.h
+++ Source/PartTimeBeat/UI/PTBResultWidget.h
@@ -3,14 +3,50 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "PTBHUDBase.h"
+#include "Blueprint/UserWidget.h"
 #include "PTBResultWidget.generated.h"
 
+class UButton;
+class UImage;
+class UTextBlock;
+
 /**
- * 
+ * 스테이지 종료 후 결과 요약을 표시하는 화면
+ * 점수/등급 표출과 다음 행동(재시도, 다음, 스토리, 종료) 분기를 담당
  */
-UCLASS()
-class PARTTIMEBEAT_API UPTBResultWidget : public UPTBHUDBase
+UCLASS(Blueprintable)
+class PARTTIMEBEAT_API UPTBResultWidget : public UUserWidget
 {
 	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "PTB|UI")
+	virtual void InitializeView();
+
+protected:
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UTextBlock> TextGrade = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UTextBlock> TextScore = nullptr;
+
+	/** 난이도에 따른 별 개수 시각화 이미지 */
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UImage> ImageStars = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UButton> ButtonRetry = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UButton> ButtonNext = nullptr;
+
+	/** 결과 후 스토리 씬으로 분기할 때 사용하는 선택 버튼 */
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UButton> ButtonStory = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UButton> ButtonExit = nullptr;
 };
