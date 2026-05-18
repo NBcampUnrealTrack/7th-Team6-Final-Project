022855b7b15f8f581b0a875f6476f28f64891934
DJKIM2002 <op302384@gmail.com>
2026-05-18 17:31:39 +0900

    feat: PTBStoryViewerWidget

--- Source/PartTimeBeat/UI/PTBStoryViewerWidget.h
+++ Source/PartTimeBeat/UI/PTBStoryViewerWidget.h
@@ -3,14 +3,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "PTBHUDBase.h"
+#include "Blueprint/UserWidget.h"
 #include "PTBStoryViewerWidget.generated.h"
 
+class UButton;
+class UImage;
+class UTextBlock;
+
 /**
- * 
+ * 스토리 대사와 캐릭터 비주얼을 순차적으로 표시하는 스토리 뷰어 위젯
  */
-UCLASS()
-class PARTTIMEBEAT_API UPTBStoryViewerWidget : public UPTBHUDBase
+UCLASS(Blueprintable)
+class PARTTIMEBEAT_API UPTBStoryViewerWidget : public UUserWidget
 {
 	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "PTB|UI")
+	virtual void InitializeView();
+
+protected:
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UTextBlock> TextDialogue = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UImage> ImageCharacter = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UButton> ButtonSkip = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UButton> ButtonNext = nullptr;
 };
