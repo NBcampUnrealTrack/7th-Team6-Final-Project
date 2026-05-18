a022e786f5bf05980fbae82a176357c20eca85b4
DJKIM2002 <op302384@gmail.com>
2026-05-18 17:31:27 +0900

    feat: PTBSettingsWidget

--- Source/PartTimeBeat/UI/PTBSettingsWidget.cpp
+++ Source/PartTimeBeat/UI/PTBSettingsWidget.cpp
@@ -1,4 +1,13 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PTBSettingsWidget.h"
+
+void UPTBSettingsWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	InitializeView();
+}
+
+void UPTBSettingsWidget::InitializeView()
+{
+}
--- Source/PartTimeBeat/UI/PTBSettingsWidget.h
+++ Source/PartTimeBeat/UI/PTBSettingsWidget.h
@@ -6,11 +6,45 @@
 #include "PTBHUDBase.h"
 #include "PTBSettingsWidget.generated.h"
 
+class UButton;
+class USlider;
+
 /**
- * 
+ * 옵션 화면 위젯
+ * 사용자 입력값을 임시 보관하고, Apply 시점에 실제 설정 반영하는 흐름을 가정
  */
-UCLASS()
+UCLASS(Blueprintable)
 class PARTTIMEBEAT_API UPTBSettingsWidget : public UPTBHUDBase
 {
 	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "PTB|UI")
+	virtual void InitializeView();
+
+protected:
+	/** 전체 출력 볼륨 조절 슬라이더 */
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<USlider> SliderMaster = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<USlider> SliderBgm = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<USlider> SliderSfx = nullptr;
+
+	/**
+	 * 판정 타이밍 보정값(오디오/입력 지연)을 조절하는 슬라이더
+	 */
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<USlider> SliderLatency = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UButton> ButtonCalibrate = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UButton> ButtonApply = nullptr;
 };
