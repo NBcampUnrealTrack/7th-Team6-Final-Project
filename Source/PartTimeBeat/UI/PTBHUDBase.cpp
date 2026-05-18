f314b1660d0b5c501f60baf42c8c80bc84458306
DJKIM2002 <op302384@gmail.com>
2026-05-18 16:51:52 +0900

    feat: PTBHUDBase 수정

--- Source/PartTimeBeat/UI/PTBHUDBase.cpp
+++ Source/PartTimeBeat/UI/PTBHUDBase.cpp
@@ -1,4 +1,18 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PTBHUDBase.h"
+
+void UPTBHUDBase::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void UPTBHUDBase::ShowHud()
+{
+	SetVisibility(ESlateVisibility::Visible);
+}
+
+void UPTBHUDBase::HideHud()
+{
+	SetVisibility(ESlateVisibility::Collapsed);
+}
--- Source/PartTimeBeat/UI/PTBHUDBase.h
+++ Source/PartTimeBeat/UI/PTBHUDBase.h
@@ -6,11 +6,29 @@
 #include "Blueprint/UserWidget.h"
 #include "PTBHUDBase.generated.h"
 
+class UButton;
+
 /**
- * 
+ * 인게임 HUD 계열 위젯의 공통 베이스 클래스
+ * - HUD 표시/숨김 정책
+ * - 공통 입력 버튼(일시정지 등) 바인딩 포인트
+ * 자식 HUD에서 재사용하기 위한 기반 역할을 담당
  */
 UCLASS()
 class PARTTIMEBEAT_API UPTBHUDBase : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "PTB|UI")
+	virtual void ShowHud();
+
+	UFUNCTION(BlueprintCallable, Category = "PTB|UI")
+	virtual void HideHud();
+
+protected:
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UButton> ButtonPause = nullptr;
 };
