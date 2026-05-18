0bd30733c3bde6449d9b650ca81aef65ff45689d
DJKIM2002 <op302384@gmail.com>
2026-05-18 17:31:53 +0900

    feat: PTBTutorialWidget

--- Source/PartTimeBeat/UI/PTBTutorialWidget.h
+++ Source/PartTimeBeat/UI/PTBTutorialWidget.h
@@ -3,14 +3,39 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "PTBHUDBase.h"
+#include "Blueprint/UserWidget.h"
 #include "PTBTutorialWidget.generated.h"
 
+class UButton;
+class UImage;
+class UTextBlock;
+
 /**
- * 
+ * 조작/규칙 안내를 단계적으로 보여주는 튜토리얼 위젯
+ * 안내 문구와 데모 이미지를 동기화해 학습 흐름을 제공
  */
-UCLASS()
-class PARTTIMEBEAT_API UPTBTutorialWidget : public UPTBHUDBase
+UCLASS(Blueprintable)
+class PARTTIMEBEAT_API UPTBTutorialWidget : public UUserWidget
 {
 	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "PTB|UI")
+	virtual void InitializeView();
+
+protected:
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UTextBlock> TextInstruction = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UImage> ImageDemo = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UButton> ButtonNext = nullptr;
+
+	UPROPERTY(meta = (BindWidgetOptional), BlueprintReadOnly, Category = "PTB|UI")
+	TObjectPtr<UButton> ButtonSkip = nullptr;
 };
