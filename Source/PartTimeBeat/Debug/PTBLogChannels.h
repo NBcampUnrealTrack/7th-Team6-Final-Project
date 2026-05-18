31b50180dbf18193a9e8986a051bb9fee92c7cff
Yahoo <sjffhdfg@gmail.com>
2026-05-18 21:19:14 +0900

    add debugSk

---
+++ Source/PartTimeBeat/Debug/PTBLogChannels.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+
+DECLARE_LOG_CATEGORY_EXTERN(LogRhythm, Log, All);
+
+DECLARE_LOG_CATEGORY_EXTERN(LogWwise, Log, All);
+
+DECLARE_LOG_CATEGORY_EXTERN(LogMulti, Log, All);
+
+DECLARE_LOG_CATEGORY_EXTERN(LogFlow, Log, All);
+
+DECLARE_LOG_CATEGORY_EXTERN(LogProfile, Log, All);
+
+DECLARE_LOG_CATEGORY_EXTERN(LogTutorial, Log, All);
