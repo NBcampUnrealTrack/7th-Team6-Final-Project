81adf974e246e005173e81894263ec78ff771099
DJKIM2002 <op302384@gmail.com>
2026-05-18 17:30:31 +0900

    feat: PTBMainTitleWidget

--- Source/PartTimeBeat/UI/PTBMainTitleWidget.cpp
+++ Source/PartTimeBeat/UI/PTBMainTitleWidget.cpp
@@ -1,4 +1,14 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PTBMainTitleWidget.h"
+
+void UPTBMainTitleWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	InitializeView();
+}
+
+void UPTBMainTitleWidget::InitializeView()
+{
+	// TODO: Bind button delegates here
+}
