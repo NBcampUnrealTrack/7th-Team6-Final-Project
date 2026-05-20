416653737b68ee85f2955f2321f19e0dcf0b1b90
limyt0 <dudx45@gmail.com>
2026-05-20 17:10:18 +0900

    PlayedrcontrollerBase, PlayerStateBase

---
+++ Source/PartTimeBeat/Core/PTBPlayerControllerBase.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PTBPlayerControllerBase.generated.h"
+
+UCLASS()
+class PARTTIMEBEAT_API APTBPlayerControllerBase : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PartTimeBeat/Core/PTBPlayerStateBase.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "PTBPlayerStateBase.generated.h"
+
+UCLASS()
+class PARTTIMEBEAT_API APTBPlayerStateBase : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
