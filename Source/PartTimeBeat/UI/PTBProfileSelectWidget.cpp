d4cec77a7bc6cf3c9d4b5fce569d1a2ec42744d1
DJKIM2002 <op302384@gmail.com>
2026-05-22 14:05:46 +0900

    feat: PTBProfileSelectWidget
    프로필 선택 레벨에 사용할 위젯

---
+++ Source/PartTimeBeat/UI/PTBProfileSelectWidget.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PTBProfileSelectWidget.h"
+#include "Components/Overlay.h"
+
+void UPTBProfileSelectWidget::NativeConstruct()
+  {
+	Super::NativeConstruct();
+
+	SetKeyboardFocus();
+
+	APlayerController* PC = GetOwningPlayer();
+	if (!PC) return;
+
+	FInputModeUIOnly InputMode;
+	InputMode.SetWidgetToFocus(TakeWidget());
+	PC->SetInputMode(InputMode);
+	PC->SetShowMouseCursor(true);
+  }
---
+++ Source/PartTimeBeat/UI/PTBProfileSelectWidget.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PTBDialogHostWidget.h"
+#include "PTBProfileSelectWidget.generated.h"
+
+/**
+ * 프로필 선택 화면의 팝업 표시와 입력 처리를 관리하는 위젯
+ */
+UCLASS()
+class PARTTIMEBEAT_API UPTBProfileSelectWidget : public UPTBDialogHostWidget
+{
+	GENERATED_BODY()
+
+protected:
+	/** 생성 시 Input Mode + 마우스 커서 자동 설정 */
+	virtual void NativeConstruct() override;
+};
+
