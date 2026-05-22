bc810937a86720fecd5f9010884d713011668342
DJKIM2002 <op302384@gmail.com>
2026-05-22 14:04:35 +0900

    feat: PTBDialogHostWidget 구현
    팝업을 필요한 위젯이 상속받는 베이스 클래스, 팝업 스택 관리

---
+++ Source/PartTimeBeat/UI/PTBDialogHostWidget.cpp
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PTBDialogHostWidget.h"
+#include "Components/Overlay.h"
+
+  void UPTBDialogHostWidget::PushDialog(UUserWidget* Dialog)
+  {
+  	if (!Dialog || !Overlay_Dialog) return;
+
+  	Overlay_Dialog->AddChildToOverlay(Dialog);
+  	DialogStack.Add(Dialog);
+  }
+
+void UPTBDialogHostWidget::CloseTopDialog()
+  {
+  	if (DialogStack.Num() == 0) return;
+
+  	UUserWidget* Top = DialogStack.Last();
+  	DialogStack.RemoveAt(DialogStack.Num() - 1);
+
+  	if (Top)
+  	{
+  		Top->RemoveFromParent();
+  		OnDialogClosed(Top);
+  	}
+  }
+
+void UPTBDialogHostWidget::CloseAllDialogs()
+  {
+  	// 역순으로 닫아야 나중에 쌓인 것부터 처리됨
+  	for (int32 i = DialogStack.Num() - 1; i >= 0; --i)
+  	{
+  		if (DialogStack[i])
+  			DialogStack[i]->RemoveFromParent();
+  	}
+  	DialogStack.Empty();
+  }
+
+bool UPTBDialogHostWidget::IsDialogOpen(TSubclassOf<UUserWidget> DialogClass) const
+  {
+  	for (const UUserWidget* Dialog : DialogStack)
+  	{
+  		if (Dialog && Dialog->IsA(DialogClass))
+  			return true;
+  	}
+  	return false;
+  }
+
+FReply UPTBDialogHostWidget::NativeOnKeyDown(
+	const FGeometry& InGeometry,
+	const FKeyEvent& InKeyEvent)
+  {
+  	if (InKeyEvent.GetKey() == EKeys::Escape && DialogStack.Num() > 0)
+  	{
+  		CloseTopDialog();
+  		return FReply::Handled();
+  	}
+  	return Super::NativeOnKeyDown(InGeometry, InKeyEvent);
+  }
