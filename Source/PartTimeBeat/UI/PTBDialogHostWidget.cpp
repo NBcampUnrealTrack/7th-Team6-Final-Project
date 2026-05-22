fef0530b3918efb2c1e88b00c55ced22895215ef
DJkim <88123944+DJKIM2002@users.noreply.github.com>
2026-05-22 14:27:19 +0900

    Refactor dialog management methods for clarity

--- Source/PartTimeBeat/UI/PTBDialogHostWidget.cpp
+++ Source/PartTimeBeat/UI/PTBDialogHostWidget.cpp
@@ -4,54 +4,54 @@
 #include "Components/Overlay.h"
 
 void UPTBDialogHostWidget::PushDialog(UUserWidget* Dialog)
-  {
-  	if (!Dialog || !Overlay_Dialog) return;
-
-  	Overlay_Dialog->AddChildToOverlay(Dialog);
-  	DialogStack.Add(Dialog);
-  }
+{
+	if (!Dialog || !Overlay_Dialog) return;
+	
+	Overlay_Dialog->AddChildToOverlay(Dialog);
+	DialogStack.Add(Dialog);
+}
 
 void UPTBDialogHostWidget::CloseTopDialog()
-  {
-  	if (DialogStack.Num() == 0) return;
-
-  	UUserWidget* Top = DialogStack.Last();
-  	DialogStack.RemoveAt(DialogStack.Num() - 1);
-
-  	if (Top)
-  	{
-  		Top->RemoveFromParent();
-  		OnDialogClosed(Top);
-  	}
-  }
+{
+	if (DialogStack.Num() == 0) return;
+	
+	UUserWidget* Top = DialogStack.Last();
+	DialogStack.RemoveAt(DialogStack.Num() - 1);
+	
+	if (Top)
+	{
+		Top->RemoveFromParent();
+		OnDialogClosed(Top);
+	}
+}
 
 void UPTBDialogHostWidget::CloseAllDialogs()
-  {
-  	// 역순으로 닫으면서 단건 닫기와 동일한 후처리(OnDialogClosed)까지 수행
-  	while (DialogStack.Num() > 0)
-  	{
-  		CloseTopDialog();
-  	}
-  }
+{
+	// 역순으로 닫으면서 단건 닫기와 동일한 후처리(OnDialogClosed)까지 수행
+	while (DialogStack.Num() > 0)
+	{
+		CloseTopDialog();
+	}
+}
 
 bool UPTBDialogHostWidget::IsDialogOpen(TSubclassOf<UUserWidget> DialogClass) const
-  {
-  	for (const UUserWidget* Dialog : DialogStack)
-  	{
-  		if (Dialog && Dialog->IsA(DialogClass))
-  			return true;
-  	}
-  	return false;
-  }
+{
+	for (const UUserWidget* Dialog : DialogStack)
+	{
+		if (Dialog && Dialog->IsA(DialogClass))
+			return true;
+	}
+	return false;
+}
 
 FReply UPTBDialogHostWidget::NativeOnKeyDown(
 	const FGeometry& InGeometry,
 	const FKeyEvent& InKeyEvent)
-  {
-  	if (InKeyEvent.GetKey() == EKeys::Escape && DialogStack.Num() > 0)
-  	{
-  		CloseTopDialog();
-  		return FReply::Handled();
-  	}
-  	return Super::NativeOnKeyDown(InGeometry, InKeyEvent);
-  }
+{
+	if (InKeyEvent.GetKey() == EKeys::Escape && DialogStack.Num() > 0)
+	{
+		CloseTopDialog();
+		return FReply::Handled();
+	}
+	return Super::NativeOnKeyDown(InGeometry, InKeyEvent);
+}
