bf9e0bf08467cb557d7ccb50a5a85800a81f2199
DJkim <88123944+DJKIM2002@users.noreply.github.com>
2026-05-22 14:25:46 +0900

    Refactor dialog management functions in PTBDialogHostWidget

--- Source/PartTimeBeat/UI/PTBDialogHostWidget.h
+++ Source/PartTimeBeat/UI/PTBDialogHostWidget.h
@@ -19,39 +19,39 @@ class PARTTIMEBEAT_API UPTBDialogHostWidget : public UUserWidget
   GENERATED_BODY()
 
 public:
-/** 팝업을 Overlay에 추가하고 스택에 쌓기 */
-UFUNCTION(BlueprintCallable, Category = "Dialog")
-void PushDialog(UUserWidget* Dialog);
-
-/** 가장 위 팝업만 닫기 */
-UFUNCTION(BlueprintCallable, Category = "Dialog")
-void CloseTopDialog();
-
-/** 열린 팝업 전부 닫기 */
-UFUNCTION(BlueprintCallable, Category = "Dialog")
-void CloseAllDialogs();
-
-/** 현재 열린 팝업 수 */
-UFUNCTION(BlueprintPure, Category = "Dialog")
-int32 GetDialogCount() const { return DialogStack.Num(); }
-
-/** 특정 팝업이 스택에 있는지 확인 */
-UFUNCTION(BlueprintPure, Category = "Dialog")
-bool IsDialogOpen(TSubclassOf<UUserWidget> DialogClass) const;
+	/** 팝업을 Overlay에 추가하고 스택에 쌓기 */
+	UFUNCTION(BlueprintCallable, Category = "Dialog")
+	void PushDialog(UUserWidget* Dialog);
+	
+	/** 가장 위 팝업만 닫기 */
+	UFUNCTION(BlueprintCallable, Category = "Dialog")
+	void CloseTopDialog();
+	
+	/** 열린 팝업 전부 닫기 */
+	UFUNCTION(BlueprintCallable, Category = "Dialog")
+	void CloseAllDialogs();
+	
+	/** 현재 열린 팝업 수 */
+	UFUNCTION(BlueprintPure, Category = "Dialog")
+	int32 GetDialogCount() const { return DialogStack.Num(); }
+	
+	/** 특정 팝업이 스택에 있는지 확인 */
+	UFUNCTION(BlueprintPure, Category = "Dialog")
+	bool IsDialogOpen(TSubclassOf<UUserWidget> DialogClass) const;
 
 protected:
-virtual FReply NativeOnKeyDown(
-	  const FGeometry& InGeometry,
-	  const FKeyEvent& InKeyEvent) override;
-
-/** Blueprint에서 팝업이 닫힐 때 추가 처리가 필요하면 오버라이드 */
-UFUNCTION(BlueprintImplementableEvent, Category = "Dialog")
-void OnDialogClosed(UUserWidget* ClosedDialog);
-
-UPROPERTY(meta = (BindWidget))
-TObjectPtr<class UOverlay> Overlay_Dialog;
+	virtual FReply NativeOnKeyDown(
+		  const FGeometry& InGeometry,
+		  const FKeyEvent& InKeyEvent) override;
+	
+	/** Blueprint에서 팝업이 닫힐 때 추가 처리가 필요하면 오버라이드 */
+	UFUNCTION(BlueprintImplementableEvent, Category = "Dialog")
+	void OnDialogClosed(UUserWidget* ClosedDialog);
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UOverlay> Overlay_Dialog;
 
 private:
-UPROPERTY()
-TArray<UUserWidget*> DialogStack;
+	UPROPERTY()
+	TArray<UUserWidget*> DialogStack;
 };
