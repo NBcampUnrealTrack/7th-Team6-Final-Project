5050a506351ab8dd73411115be57c7a1d9577ed1
Yahoo <sjffhdfg@gmail.com>
2026-05-28 12:10:19 +0900

    add:file
    이동할 물고기파일추가

---
+++ Source/PartTimeBeat/MiniGames/FS/FishActor.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FishActor.h"
+
+
+// Sets default values
+AFishActor::AFishActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AFishActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFishActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
