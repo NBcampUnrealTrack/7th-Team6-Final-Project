45eab7da2250dc0ad8989fe3a034b92402d82f99
Yahoo <sjffhdfg@gmail.com>
2026-05-28 11:34:12 +0900

    add:file
    파일생성후 커밋

---
+++ Source/PartTimeBeat/MiniGames/FS/PTBFSMiniGame.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MiniGames/Common/PTBBaseMiniGame.h"
+#include "PTBFSMiniGame.generated.h"
+
+UCLASS()
+class PARTTIMEBEAT_API APTBFSMiniGame : public APTBBaseMiniGame
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APTBFSMiniGame();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
