4c835095bce9089cd9e840a5a21ef1ef760c5451
Yahoo <sjffhdfg@gmail.com>
2026-06-01 16:28:49 +0900

    add:file FSMiniGameModeAdd

---
+++ Source/PartTimeBeat/MiniGames/FS/FSGamMode.cpp
@@ -0,0 +1,64 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FSGamMode.h"
+#include "FishActor.h"
+#include "FSGamMode.h"
+#include "PTBFSMiniGame.h"
+
+AFSGameMode::AFSGameMode()
+{
+	SpawnedFishActor = nullptr;
+	FishingMiniGame = nullptr;
+	HUDWidget = nullptr;
+}
+
+void AFSGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	FVector FishActorLocation = FVector::ZeroVector;
+	FRotator FishActorRotation = FRotator::ZeroRotator;
+	SpawnedFishActor = GetWorld()->SpawnActor<AFishActor>(FishActorClass, FishActorLocation, FishActorRotation);
+	
+	FVector FSGameActorLocation = FVector::ZeroVector;
+	FRotator FSGameActorRotation = FRotator::ZeroRotator;
+	FishingMiniGame = GetWorld()->SpawnActor<APTBFSMiniGame>(MiniGameClass,FSGameActorLocation,FSGameActorRotation);
+	
+	if (FishingMiniGame)
+	{
+		FishingMiniGame->OnMiniGameFinished.AddDynamic(
+			this, &AFSGameMode::HandleFishingMiniGameFinished);
+	}
+	
+	APlayerController* PC = GetWorld()->GetFirstPlayerController();
+	if (PC && HUDWidgetClass)
+	{
+		HUDWidget = CreateWidget<UUserWidget>(PC, HUDWidgetClass);
+		if (HUDWidget)
+		{
+			HUDWidget->AddToViewport();
+		}
+	}
+	
+}
+
+void AFSGameMode::HandleFishingMiniGameFinished(FPTBRoundResult Result)
+{
+	if (SpawnedFishActor)
+	{
+		SpawnedFishActor->Destroy();
+		SpawnedFishActor = nullptr;
+	}
+	
+	if (FishingMiniGame)
+	{
+		FishingMiniGame->Destroy();
+		FishingMiniGame = nullptr;
+	}
+	
+	if (HUDWidget)
+	{
+		HUDWidget->RemoveFromParent();
+		HUDWidget = nullptr;
+	}
+}
---
+++ Source/PartTimeBeat/MiniGames/FS/FSGamMode.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/PTBGameModeBase.h"
+#include "FSGamMode.generated.h"
+
+/**
+ * 
+ */
+class APTBFSMiniGame;
+class AFishActor;
+
+
+UCLASS()
+class PARTTIMEBEAT_API AFSGameMode : public APTBGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AFSGameMode();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FS|Spawn")
+	TSubclassOf<APTBFSMiniGame> MiniGameClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FS|Spawn")
+	TSubclassOf<AFishActor> FishActorClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FS|UI")
+	TSubclassOf<UUserWidget> HUDWidgetClass;
+
+private:
+	UPROPERTY()
+	TObjectPtr<APTBFSMiniGame> FishingMiniGame;
+
+	UPROPERTY()
+	TObjectPtr<AFishActor> SpawnedFishActor;
+
+	UPROPERTY()
+	TObjectPtr<UUserWidget> HUDWidget;
+
+	UFUNCTION()
+	void HandleFishingMiniGameFinished(FPTBRoundResult Result);
+};
