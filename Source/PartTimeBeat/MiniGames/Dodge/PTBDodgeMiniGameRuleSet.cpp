84548d1f66cf4c23298a945136520b0cafd05448
rain <junhee030707@gmail.com>
2026-06-01 17:21:42 +0900

    코드작업2

---
+++ Source/PartTimeBeat/MiniGames/Dodge/PTBDodgeMiniGameRuleSet.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MiniGames/Dodge/PTBDodgeMiniGameRuleSet.h"
+
+UPTBDodgeMiniGameRuleSet::UPTBDodgeMiniGameRuleSet()
+{
+    // 점프 입력만 허용
+    SupportedActions.Add(EPTBActionType::ActionA);
+}
---
+++ Source/PartTimeBeat/MiniGames/Dodge/PTBDodgeMiniGameRuleSet.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Core/PTBStructEnums.h"
+#include "PTBDodgeMiniGameRuleSet.generated.h"
+
+/**
+ * 리듬 피하기 미니게임 규칙 정의
+ */
+UCLASS()
+class PARTTIMEBEAT_API UPTBDodgeMiniGameRuleSet : public UDataAsset
+{
+    GENERATED_BODY()
+
+public:
+    UPTBDodgeMiniGameRuleSet();
+
+    // 미니게임 ID (채보 JSON 과 반드시 일치)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PTB|Dodge")
+    FName MiniGameId = FName("Dodge");
+
+    // 허용 입력 (점프만)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PTB|Dodge")
+    TArray<EPTBActionType> SupportedActions;
+
+    // Easy 난이도 LookAhead Beats
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PTB|Dodge")
+    float LookAheadBeatsEasy = 2.0f;
+
+    // Standard 난이도 LookAhead Beats
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PTB|Dodge")
+    float LookAheadBeatsStandard = 1.0f;
+
+    // Insane 난이도 LookAhead Beats
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PTB|Dodge")
+    float LookAheadBeatsInsane = 0.5f;
+
+    // 체력 감소량
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PTB|Dodge")
+    int32 HealthDecreaseAmount = 10;
+
+    // 최대 미스 횟수
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PTB|Dodge")
+    int32 MaxMissCount = 10;
+};
