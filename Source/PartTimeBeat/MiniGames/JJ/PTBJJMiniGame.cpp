b6242051d4a0c21d9ea14bc76a2bb7f83fbac4ef
limyt0 <dudx45@gmail.com>
2026-06-02 16:47:51 +0900

    Add: PTBJJMiniGame

---
+++ Source/PartTimeBeat/MiniGames/JJ/PTBJJMiniGame.cpp
@@ -0,0 +1,1 @@
+#include "MiniGames/JJ/PTBJJMiniGame.h"
---
+++ Source/PartTimeBeat/MiniGames/JJ/PTBJJMiniGame.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MiniGames/Common/PTBBaseMiniGame.h"
+#include "PTBJJMiniGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARTTIMEBEAT_API APTBJJMiniGame : public APTBBaseMiniGame
+{
+	GENERATED_BODY()
+	
+};
