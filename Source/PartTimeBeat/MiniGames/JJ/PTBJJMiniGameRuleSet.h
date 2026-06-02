52e820650b0dc922f79a1db403da8cc20d3de79e
limyt0 <dudx45@gmail.com>
2026-06-02 16:32:16 +0900

    Add: PTBJJMiniGameRuleSet

---
+++ Source/PartTimeBeat/MiniGames/JJ/PTBJJMiniGameRuleSet.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MiniGames/Common/PTBMiniGameRuleSet.h"
+#include "PTBJJMiniGameRuleSet.generated.h"
+
+
+UCLASS()
+class PARTTIMEBEAT_API UPTBJJMiniGameRuleSet : public UPTBMiniGameRuleSet
+{
+	GENERATED_BODY()
+	
+};
