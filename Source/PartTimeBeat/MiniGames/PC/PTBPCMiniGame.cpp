0c9f9b042fed0daef7cc10415ff91fdb3c3de0b0
mio970407-dot <mio970407@gmail.com>
2026-06-09 19:40:36 +0900

    BP,C++수정

--- Source/PartTimeBeat/MiniGames/PC/PTBPCMiniGame.cpp
+++ Source/PartTimeBeat/MiniGames/PC/PTBPCMiniGame.cpp
@@ -1,9 +1,45 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "MiniGames/PC/PTBPCMiniGame.h"
+﻿#include "MiniGames/PC/PTBPCMiniGame.h"
 
 APTBPCMiniGame::APTBPCMiniGame()
 {}
 
-APTBPCMiniGame();
+void APTBPCMiniGame::BuildRuntimeState()
+{
+    Super::BuildRuntimeState();
+
+}
+
+void APTBPCMiniGame::HandleNoteCue(FPTBNoteEvent Note)
+{
+    Super::HandleNoteCue(Note);
+
+    SpawnCueForNote(Note);
+}
+
+void APTBPCMiniGame::HandleNoteArm(FPTBNoteEvent Note)
+{
+    Super::HandleNoteArm(Note);
+
+}
+
+void APTBPCMiniGame::HandleChartEvent(FPTBNoteEvent Note)
+{
+    Super::HandleChartEvent(Note);
+
+}
+
+void APTBPCMiniGame::HandleJudgementResult(FPTBJudgementResult Result)
+{
+    Super::HandleJudgementResult(Result);
+
+    ClearCueForJudgement(Result);
+
+}
+
+void APTBPCMiniGame::SpawnCueForNote(const FPTBNoteEvent& Note)
+{
+}
+
+void APTBPCMiniGame::ClearCueForJudgement(const FPTBJudgementResult& Result)
+{
+}
--- Source/PartTimeBeat/MiniGames/PC/PTBPCMiniGame.h
+++ Source/PartTimeBeat/MiniGames/PC/PTBPCMiniGame.h
@@ -19,35 +19,13 @@ public:
 
 protected:
 
-	/** 오브젝트 / 상태 구성(하위 override) */
-	void BuildRuntimeState() override;
+    virtual void BuildRuntimeState() override;
+    virtual void HandleNoteCue(FPTBNoteEvent Note) override;
+    virtual void HandleNoteArm(FPTBNoteEvent Note) override;
+    virtual void HandleChartEvent(FPTBNoteEvent Note) override;
+    virtual void HandleJudgementResult(FPTBJudgementResult Result) override;
 
-	/** 선행 비주얼 큐(하위 override) */
-	UFUNCTION()
-	void HandleNoteCue(FPTBNoteEvent Note);
-
-	/** 판정 등록 가능 상태 진입(하위 override) */
-	UFUNCTION()
-	void HandleNoteArm(FPTBNoteEvent Note) override;
-
-	/** 채보 이벤트 처리(하위 override) */
-	UFUNCTION()
-	void HandleChartEvent(FPTBNoteEvent Note) override;
-
-	/** 점수 / HUD / SFX 반영 */
-	UFUNCTION()
-	void HandleJudgementResult(FPTBJudgementResult Result) override;
-
-	/** 미니게임 전용 피드백(하위 override) */
-	void PlayJudgementFeedback(const FPTBJudgementResult& Result) override;
-
-	/** 전용 결과(하위 override) */
-	virtual FPTBMiniGameResultPayload BuildResultPayload() const override;
-
-	/** 라운드 시작 전 에셋 준비(하위 override 권장) */
-	void PreloadAssets() override;
-	
-	/** 오디오 에셋 준비 */
-	void PreloadAudioAssets() override;
-
+private:
+    void SpawnCueForNote(const FPTBNoteEvent& Note);
+    void ClearCueForJudgement(const FPTBJudgementResult& Result);
 };
