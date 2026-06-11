2093dbee0a7d20534056c18672fbea90af973fcf
Nichipe-JCM <cjfals4539@gmail.com>
2026-06-11 19:23:59 +0900

    fix: UFUNCTION 버그 수정

--- Source/PartTimeBeat/MiniGames/Common/PTBBaseMiniGame.h
+++ Source/PartTimeBeat/MiniGames/Common/PTBBaseMiniGame.h
@@ -86,7 +86,6 @@ public:
 	FPTBOnMiniGameStarted OnMiniGameStarted;
 
 	/** 컨텍스트 주입 */
-	UFUNCTION(BlueprintCallable, Category = "PTB|MiniGame")
 	virtual void InitializeMiniGame(const FPTBMiniGameContext& Context);
 
 	/** BGM + Conductor 시작, 입력 허용 */
