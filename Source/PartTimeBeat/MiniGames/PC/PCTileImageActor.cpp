f560db590b80be47baf66c7169812b1db774af89
mio970407-dot <mio970407@gmail.com>
2026-06-23 18:08:40 +0900

    BP추가

---
+++ Source/PartTimeBeat/MiniGames/PC/PCTileImageActor.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include "MiniGames/PC/PCTileImageActor.h"
+#include "Materials/MaterialInstanceDynamic.h"
+
+APCTileImageActor::APCTileImageActor()
+{
+    PlaneMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PlaneMesh"));
+    RootComponent = PlaneMesh;
+
+    // 기본 플레인 메시 설정
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> Plane(TEXT("/Engine/BasicShapes/Plane"));
+    if (Plane.Succeeded())
+        PlaneMesh->SetStaticMesh(Plane.Object);
+}
+
+void APCTileImageActor::SetTexture(UTexture2D* Texture)
+{
+    if (!Texture) return;
+
+    // 에디터에서 BP_PCTileImageActor에 머티리얼 연결 필요
+    if (!DynamicMaterial)
+    {
+        DynamicMaterial = PlaneMesh->CreateAndSetMaterialInstanceDynamic(0);
+    }
+    DynamicMaterial->SetTextureParameterValue(TEXT("Texture"), Texture);
+}
+
+void APCTileImageActor::BeginPlay()
+{
+    Super::BeginPlay();
+    UE_LOG(LogTemp, Warning, TEXT("TileImageActor BeginPlay 호출됨"));
+}
---
+++ Source/PartTimeBeat/MiniGames/PC/PCTileImageActor.h
@@ -0,0 +1,29 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PCTileImageActor.generated.h"
+
+UCLASS()
+class PARTTIMEBEAT_API APCTileImageActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APCTileImageActor();
+
+	void SetTexture(UTexture2D* Texture);
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* PlaneMesh;
+
+	UPROPERTY()
+	UMaterialInstanceDynamic* DynamicMaterial;
+
+protected:
+	virtual void BeginPlay() override;
+};
