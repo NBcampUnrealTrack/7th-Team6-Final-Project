7af9ff37ae9ea813bea2d2e6765449812da971eb
Yahoo <sjffhdfg@gmail.com>
2026-06-26 20:47:40 +0900

    add: 물고기 랜덤스폰을 위한 구조체 생성

---
+++ Source/PartTimeBeat/MiniGames/FS/FFSSkFish.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "FFSSkFish.generated.h"
+
+USTRUCT(BlueprintType)
+struct FFFSSkFish : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USkeletalMesh> FishMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UAnimMontage> SwimMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UAnimMontage> IdleMontage;
+};
